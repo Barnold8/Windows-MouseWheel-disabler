d61c49b0a19b4ec109106484de764e2e412abf3c
Brandon Wright <bandslover@outlook.com>
2024-06-11 21:58:10 +0100

    Convert to windows application

--- scrollDisable/scrollDisable/Source.cpp
+++ scrollDisable/scrollDisable/Source.cpp
@@ -1,36 +1,28 @@
 #include <windows.h>
 #include <iostream>
 
-
 HHOOK hMouseHook;
-
 
 LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
 
-    if(nCode < 0){
+    if (nCode < 0) {
         return CallNextHookEx(hMouseHook, nCode, wParam, lParam);
     }
 
     if (wParam == WM_MOUSEWHEEL) {
-            return 1;
-        }
+        return 1;
+    }
 
     return CallNextHookEx(hMouseHook, nCode, wParam, lParam);
 }
 
-
-int main() {
-
-    SetWindowsHookEx(WH_MOUSE_LL,MouseProc, GetModuleHandle(NULL),0);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,PSTR lpCmdLine, int nCmdShow)
+{
+    SetWindowsHookEx(WH_MOUSE_LL, MouseProc, GetModuleHandle(NULL), 0);
 
     MSG msg;
 
-    while(GetMessage(&msg,NULL,0,0)){
-
-        
-
-    }
-
+    while (GetMessage(&msg, NULL, 0, 0)) {}
 
     return 0;
 }
